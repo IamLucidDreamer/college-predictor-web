184e3e221a5dd9f91064c6b8d85894727ff15b9b
Jerome-Flynaut <jeromejoseph782@gmail.com>
2023-05-05 17:19:06 +0530

    dynamic data added in College profile

--- src/views/dashboardPages/CollegeProfile.jsx
+++ src/views/dashboardPages/CollegeProfile.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { server } from "../../helpers/apiCall";
 
 const CollegeProfile = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = () => {
+    server.get('college/get-all')
+    .then((res) => setData(res.data.data.College[0],"This is res"))
+    .catch((err) => console.log(err))
+  }
+
+  console.log(data,"This is data")
+
   return (
-    <div class="bg-gray-100">
-      <div className="bg-blue-900 p-20 pb-10 text-white text-center">
-        <img className="mx-auto w-40 h-40 border-4 border-white" src="https://img.collegepravesh.com/2016/06/IIT-Bhubaneswar-Logo.png" alt="" />
-        <h1 className="mt-5 text-2xl font-semibold">Indian Institute of Technology, Bhubaneswar</h1>
-        <h2 className="mt-2 text-xs">Indian Institute of Technology, Argul Campus, Jatani, Khordha, Bhubaneswar, Odisha - 752050</h2>
+    <div className="bg-gray-100">
+      <div className="bg-blue-500 p-20 pb-10 text-white text-center">
+        <img className="mx-auto w-40 h-40 border-4 border-white" src={data.collegeIcon} alt="" />
+        <h1 className="mt-5 text-2xl font-semibold">{data.collegeName}</h1>
+        {/* <h2 className="mt-2 text-xs">Indian Institute of Technology, Argul Campus, Jatani, Khordha, Bhubaneswar, Odisha - 752050</h2> */}
       </div>
       <div class="container mx-auto my-5 p-5">
         <div class="md:flex no-wrap md:-mx-2 ">
@@ -127,29 +144,74 @@ const CollegeProfile = () => {
                 <div class="grid md:grid-cols-2 text-sm">
                   <div class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">Institute Name</div>
-                    <div class="px-4 py-2">Indian Institute of Technology, Bhubaneswar</div>
+                    <div class="px-4 py-2">{data.collegeName}</div>
                   </div>
                   <div class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">Also Known As</div>
-                    <div class="px-4 py-2">IIT BBS</div>
+                    <div class="px-4 py-2">{data.collegeTag}</div>
                   </div>
                   <div class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">Institute Type</div>
-                    <div class="px-4 py-2">Government</div>
+                    <div class="px-4 py-2">{data.collegeType}</div>
                   </div>
                   <div class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">Established</div>
-                    <div class="px-4 py-2">2008</div>
+                    <div class="px-4 py-2">{data.estYear}</div>
                   </div>
                   <div class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">Location</div>
-                    <div class="px-4 py-2">Bhubaneswar, Odisha</div>
+                    <div class="px-4 py-2">{data.state}</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Contact Number</div>
+                    <div class="px-4 py-2">{data.contactNumber}</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Hotness Score</div>
+                    <div class="px-4 py-2">{data.hotnessScore}</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Ranking</div>
+                    <div class="px-4 py-2">{data.ranking}</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Website</div>
+                    <div class="px-4 py-2">{data.website}</div>
                   </div>
                 </div>
               </div>
               <button class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">
                 Show Full Information
               </button>
+            </div>
+
+            <div class="my-4"></div>
+
+            <div class="bg-white p-3 shadow-sm rounded-sm">
+              <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+                <span clas="text-green-500">
+                  <svg
+                    class="h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                    />
+                  </svg>
+                </span>
+                <span class="tracking-wide">Campus Photos</span>
+              </div>
+              <div className="mt-2">
+                {data.campusPhotos?.map((item) => (
+                  <img key={item} className="border w-40 h-40" src={item} alt="" />
+                ))}
+              </div>
             </div>
 
             <div class="my-4"></div>
@@ -177,24 +239,11 @@ const CollegeProfile = () => {
                     <span class="tracking-wide">COURSES OFFERED</span>
                   </div>
                   <ul class="list-inside space-y-2">
-                    <li>
-                      <div class="text-teal-600">Civil Engineering</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Computer Science and Engineering</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Electrical Engineering</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Electronics and Communication Engineering</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Mechanical Engineering</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Metallurgical and Materials Engineering</div>
-                    </li>
+                    {data?.coursesOffered?.map((item) => (
+                      <li key={item}>
+                        <div class="text-teal-600">{item}</div>
+                      </li>
+                    ))}
                   </ul>
                 </div>
                 <div>
